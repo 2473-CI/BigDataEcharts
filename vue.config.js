d6344535413df85854fb6e56b4caf4f1978481de
yr@qq.com <yr@qq.com>
2023-02-02 18:30:56 +0800

    代理更改

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,17 @@ module.exports = defineConfig({
   // publicPath: "./",
   transpileDependencies: true,
   lintOnSave:false,
-  // devServer:{
-  //   port: 8082,
-  //   proxy:{
-  //     '/api':{
-  //       // target:'http://192.168.45.5:14732/',
-  //       target:'http://127.0.0.1:8999/',
-  //       changeOrigin: true,
-  //       pathRewrite:{
-  //         '^/api':''
-  //       }
-  //     }
-  //   }
-  // }
+  devServer:{
+    port: 8082,
+    proxy:{
+      '/api':{
+        // target:'http://192.168.45.5:14732/',
+        target:'http://127.0.0.1:8999/',
+        changeOrigin: true,
+        pathRewrite:{
+          '^/api':''
+        }
+      }
+    }
+  }
 })
