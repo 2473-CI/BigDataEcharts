fdfbd2686b13255ab7617ae0537747ba93ddedbc
yr <yr@gmail.com>
2023-02-23 12:02:16 +0800

    日本地震分析完成

--- src/view/earthQuakes.vue
+++ src/view/earthQuakes.vue
@@ -7,7 +7,7 @@
         </h6>
         <div class="char" ref="k1"></div>
         <h6>
-            2. 日本每月各等级地震发生次数 （叠加柱状）
+            2. 日本每月各等级地震发生次数
         </h6>
         <div class="char" ref="k2"></div>
         <h6>
@@ -15,7 +15,7 @@
         </h6>
         <div class="char" ref="k3"></div>
         <h6>
-           4. 各时刻发生地震的可能性 （时间轴 折线）
+           4. 各时刻发生地震的可能性 （折线）
         </h6>
         <div class="char" ref="k4"></div>
         <h6>
@@ -37,7 +37,125 @@ const k3 = ref("k3")
 const k4 = ref("k4")
 const k5 = ref("k5")
 
-onMounted(()=>{
+onMounted(async ()=>{
+    await axios.get("/api/EarthQuakes").then(res => data=res.data)
+    data = data.map(raw => {
+        function d(str){
+            let splitArr = str.replace("°", "′").split("′")
+            return Number(splitArr[0]) < 0 ? Number(splitArr[0]) - (Number(splitArr[1])/60) : Number(splitArr[0]) + (Number(splitArr[1])/60) 
+        }
+        return {
+            ...raw,
+            "强度": Number(raw["强度"]),
+            "深度": Number(raw["深度"].split(" ")[0]),
+            "经度": d(raw["精度"]),
+            "纬度": d(raw["纬度"]),
+            "月": raw["日期"].split("/")[1],
+            "时间": raw["时间"].split(".")[1]
+        }
+    })
+    console.log(data)
+    
+    let time = [...new Set(data.map(raw => raw["时间"]))].sort((o1, o2) => o1-o2)
+    let month = [...new Set(data.map(raw=>raw["月"]))].sort((o1, o2) => Number(o1)-Number(o2))
+    let grade = [...new Set(data.map(raw => raw["最大震度"]))]
+    let depth = [...new Set(data.map(raw => raw["深度"]))].sort((o1, o2) => o1-o2)
+
+    let plot1 = echarts.init(k1.value)
+    let opt1 = {
+        xAxis: {},
+        yAxis: {},
+        series: [
+            {
+            symbolSize: 10,
+            data: data.map(raw => [raw["经度"], raw["纬度"]]),
+            type: 'scatter'
+            }
+        ]
+    }
+    plot1.setOption(opt1)
+
+    let tmp2 = month.map(m=>{
+        return grade.map(g => {
+            return data.filter(raw => raw["月"]==m && raw["最大震度"] == g).length
+        })
+    })
+
+    let plot2 = echarts.init(k2.value)
+    let opt2 = {
+        xAxis: {data: month},
+        yAxis: {},
+        series: grade.map((g, i) => {
+            return {
+                type: "bar",
+                name: g,
+                // stack: "s",
+                data: tmp2.map(raw => raw[i])
+            }
+        }),
+        legend: {
+            data: grade
+        }
+    }
+    plot2.setOption(opt2)
+
+    function multiplicity(arr){
+        return [...new Set(arr)].map(n=>{return{name:n, value:arr.filter(o=>o==n).length}}).sort((o1, o2) => o2.value-o1.value)[0]["name"]
+    }
+
+    let plot3 = echarts.init(k3.value)
+    let opt3 = {
+        xAxis: {data: depth},
+        yAxis: {},
+        series: [
+            {
+                type: "line",
+                data: depth.map(d=>multiplicity(data.filter(raw=>raw["深度"]==d).map(raw=>raw["强度"])))
+            }
+        ]
+    }
+    plot3.setOption(opt3)
+
+    let plot4 = echarts.init(k4.value) 
+    let opt4 = {
+        xAxis: {
+            data: time
+        },
+        yAxis: {},
+        series: [
+            {
+                type: "line",
+                data: time.map(t=> data.filter(raw=>raw["时间"]==t).length/data.length)
+            }
+        ],
+    }
+    console.log(opt4)
+    plot4.setOption(opt4)
+
+    let plot5 = echarts.init(k5.value)
+    let opt5 = {
+        legend: {
+            data: grade
+        },
+        series: [
+            {
+                type: "pie",
+                data: grade.map(g => {
+                    return {
+                        name: g,
+                        value: data.filter(raw=>raw["最大震度"]==g).length
+                    }
+                }),
+                label: {
+                    normal: {
+                        show: true,
+                        formatter: '{b}\n{d}%',
+                    }
+                }
+            }
+        ]
+    }
+    plot5.setOption(opt5)
     
 })
 
